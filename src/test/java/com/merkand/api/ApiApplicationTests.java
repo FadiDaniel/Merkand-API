95a463241b267820cdb32f05b08dcce261ca0330
FadiDaniel <fadielzidi@gmail.com>
2025-12-05 18:29:04 +0100

    first commit

---
+++ src/test/java/com/merkand/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merkand.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
